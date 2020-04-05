78c001aca6e9b3686060ad67d698521d04d7d00c
gabe <gabrielbruck1@gmail.com>
2020-04-05 20:09:26 +0300

    Milestone6and7

--- Excercise5.py
+++ Excercise5.py
@@ -1,14 +1,36 @@
+#
+# def build_pyramid():
+#     num = int(input("How many rows:"))
+#     if num <= 0:
+#         print("Please enter a positive Number")
+#     else:
+#         for i in range (0,num):
+#              for j in range(0,num-i-1):
+#                 print(end="  ")
+#              for j in range(0,2*i+1):
+#                 print("*", end=" ")
+#              print()
+#
+# build_pyramid()
 
 def build_pyramid():
-    num = 5
-    if num <= 0:
-        print("Positive Number")
-    else:
+    num = int(input("How many rows:"))
+    upordown = input("please type up or down:")
+    if num > 0 and upordown == "up":
         for i in range (0,num):
              for j in range(0,num-i-1):
                 print(end="  ")
              for j in range(0,2*i+1):
                 print("*", end=" ")
              print()
+    elif num > 0 and upordown == "down":
+        for i in reversed(range(0, num)):
+                for j in range(0, num - i - 1):
+                    print(end="  ")
+                for j in range(0, 2 * i + 1):
+                    print("*", end=" ")
+                print()
+    else:
+        print("Invalid")
 
 build_pyramid()
